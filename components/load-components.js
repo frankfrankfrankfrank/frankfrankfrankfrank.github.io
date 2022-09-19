fc4cb21916cd64799eb3583b63966b2332968b7e
Frank! <franktstewart@gmail.com>
2022-09-19 17:41:57 -0400

    Breakout test

--- components/load-components.js
+++ components/load-components.js
@@ -2,4 +2,6 @@ $(function(){
   $("#header").load("header.html");
   $("#footer").load("footer.html");
   $("#dnd").load("dnd.html");
+  $("#breakout").load("breakout.html");
+  $("#jump").load("jump.html");
 });
