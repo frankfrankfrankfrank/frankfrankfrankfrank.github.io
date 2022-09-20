f0f94f884b3c51a088856cf05022f2f5a62a62ec
Frank! <franktstewart@gmail.com>
2022-09-19 20:26:34 -0400

    Breakout and component section updates (structural / HTML)

--- _site/components/load-components.js
+++ _site/components/load-components.js
@@ -3,5 +3,5 @@ $(function(){
   $("#footer").load("footer.html");
   $("#dnd").load("dnd.html");
   $("#breakout").load("breakout.html");
-  $("#jump").load("jump.html");
+  $("#cards").load("cards.html");
 });
