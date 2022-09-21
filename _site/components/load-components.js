378a61f4e07c87d847ab9dc56031e549f02c2fb6
Frank! <franktstewart@gmail.com>
2022-09-21 17:26:03 -0400

    Tabs section, Contact section, Research article screen

--- _site/components/load-components.js
+++ _site/components/load-components.js
@@ -1,7 +1,7 @@
 $(function(){
   $("#header").load("header.html");
   $("#footer").load("footer.html");
-  $("#dnd").load("dnd.html");
-  $("#breakout").load("breakout.html");
   $("#cards").load("cards.html");
+  $("#contact").load("contact.html");
+  $("#studies").load("case-studies.html");
 });
