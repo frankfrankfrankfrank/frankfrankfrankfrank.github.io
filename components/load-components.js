4110fc81298c42cd7123d7569a9d1d4aa735129f
Frank! <franktstewart@gmail.com>
2022-09-21 20:14:32 -0400

    Resaerch article draft with 'back to top' link

--- components/load-components.js
+++ components/load-components.js
@@ -1,7 +1,7 @@
 $(function(){
   $("#header").load("header.html");
-  $("#footer").load("footer.html");
   $("#cards").load("cards.html");
-  $("#contact").load("contact.html");
   $("#studies").load("case-studies.html");
+  $("#contact").load("contact.html");
+  $("#footer").load("footer.html");
 });
