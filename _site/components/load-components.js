3e7b54174c704c663c867881214956beb4a4c3a8
Frank! <franktstewart@gmail.com>
2022-10-05 21:08:58 -0400

    Secondary screens and animation updates. Updates to Nav links.

--- _site/components/load-components.js
+++ _site/components/load-components.js
@@ -3,5 +3,6 @@ $(function(){
   $("#cards").load("cards.html");
   $("#studies").load("case-studies.html");
   $("#contact").load("contact.html");
+  $("#survey").load("survey.html");
   $("#footer").load("footer.html");
 });
