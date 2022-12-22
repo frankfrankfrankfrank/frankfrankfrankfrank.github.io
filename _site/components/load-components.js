ad7d735165aab3496747ba37c240a13ef42797a9
Frank! <franktstewart@gmail.com>
2022-12-22 12:51:00 -0500

    Case study concept updated (removed case study tabs from index). Copy edits for all screens. Updated images to match new style.

--- _site/components/load-components.js
+++ _site/components/load-components.js
@@ -1,7 +1,7 @@
 $(function(){
   $("#header").load("header.html");
   $("#cards").load("cards.html");
-  $("#studies").load("case-studies.html");
+  $("#studies").load("avap-cs.html");
   $("#contact").load("contact.html");
   $("#survey").load("survey.html");
   $("#footer").load("footer.html");
