dce63d4dc3dab6feb40231bcf11be5d520149586
Frank! <franktstewart@gmail.com>
2023-11-17 12:45:31 -0500

    The purple update
    Switched dark background to purple.
    
    Edited copy and images on index.
    
    Added an About screen.
    
    Reformatted the footer.
    
    Edited interior screens to match new color scheme.

--- js/load-components.js
+++ js/load-components.js
@@ -1,5 +1,6 @@
 $(function(){
   $("#header").load("header.html");
+  $("#tetris").load("tetris.html");
   $("#studies").load("studies.html");
   $("#logos").load("logos.html");  
   $("#value").load("value.html");
